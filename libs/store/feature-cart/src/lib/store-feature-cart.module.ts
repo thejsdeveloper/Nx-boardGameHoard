f1e3491db35b8aae02a9cc49face3a2fee90e410
vikas yadav <thejsdeveloper@gmail.com>
2021-11-09 22:25:00 +0530

    store app and lib

---
+++ libs/store/feature-cart/src/lib/store-feature-cart.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CartComponent } from './cart/cart.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [
+    CartComponent
+  ],
+  exports: [
+    CartComponent
+  ],
+})
+export class StoreFeatureCartModule {}
