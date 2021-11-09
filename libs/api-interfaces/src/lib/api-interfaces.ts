0b5eb7613c28e052628d637e5847f363aafc1524
vikas yadav <thejsdeveloper@gmail.com>
2021-11-09 20:12:45 +0530

    adding api

--- libs/api-interfaces/src/lib/api-interfaces.ts
+++ libs/api-interfaces/src/lib/api-interfaces.ts
@@ -1,3 +1,26 @@
-export interface Message {
-  message: string;
+export interface Game {
+  name: string;
+  id: string;
+  image?: string;
+  description: string;
+  price: number;
+  rating?: number;
+  reviews?: Array<Review>;
+}
+
+export interface Test {
+  nums: Array<number>;
+  strings: Array<string>;
+  // comment: boolean[];
+}
+
+export interface Review {
+  game: string;
+  rating: number;
+  content: string;
+}
+
+export interface CartItem {
+  game: string;
+  quantity: number;
 }
