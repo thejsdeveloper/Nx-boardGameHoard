b2d1d1775f5d9a799a028b84de7ba92e15857c46
vikas yadav <thejsdeveloper@gmail.com>
2021-11-09 20:45:26 +0530

    adding function

--- libs/review/util-formatter/src/lib/review-util-formatter.ts
+++ libs/review/util-formatter/src/lib/review-util-formatter.ts
@@ -1,3 +1,3 @@
-export function reviewUtilFormatter(): string {
-  return 'review-util-formatter';
-}
+export const currencyFormat = (amount: number) => '$' + amount.toFixed(2);
+export const ratingFormat = (rating: number, maxRating: number) =>
+  (rating === undefined ? '?' : rating.toFixed(0)) + '/' + maxRating;
